a0582983bf07c6238cac7308562aa4ccad566ec1
cesjul <juliocesarsouzacarvalho284@gmail.com>
2026-06-23 20:42:26 -0300

    test: test of stringIsNumber method

--- src/test/kotlin/stringIsNumberTest.kt
+++ src/test/kotlin/stringIsNumberTest.kt
@@ -1,7 +1,11 @@
 import expections.stringIsNumber
 import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Assumptions
+import org.junit.jupiter.api.Disabled
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+import kotlin.test.fail
 
 class StringIsNumberTest {
     @Test
@@ -12,5 +16,33 @@ class StringIsNumberTest {
             {Assertions.assertFalse(stringIsNumber("j"))},
             {Assertions.assertFalse(stringIsNumber(""))}
         )
+    }
+
+    @Test
+    @Disabled
+    fun toBeImplemented(){
+
+    }
+
+    @Test
+    fun willFail(){
+        fail("This test will fail")
+    }
+
+    @Test
+    fun conditionalTest(){
+        Assumptions.assumeTrue(stringIsNumber("1"))
+
+        Assertions.assertFalse { stringIsNumber("i") }
+    }
+
+    @Test
+    fun throws(){
+        assertThrows<NotImplementedError> { throw NotImplementedError("") }
+    }
+
+    @Test
+    fun notNull(){
+        Assertions.assertNotNull(stringIsNumber("1"))
     }
 }
